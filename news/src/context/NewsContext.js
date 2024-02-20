068de4133f023657da03e2f8d10550c50044eb33
gouribhise <gouri.a.bhise@gmail.com>
2024-02-20 17:46:59 +0530

    removed imp file

--- news/src/context/NewsContext.js
+++ news/src/context/NewsContext.js
@@ -1,11 +1,19 @@
-import React, { useContext, createContext, useState } from "react";
+import React, { useContext, createContext, useState, useEffect } from "react";
 
 const NewsContext = createContext();
-const API_KEY = process.env.REACT_APP_KEY;
+const API_KEY = `${process.env.REACT_APP_KEY}`;
 const url = `GET https://newsapi.org/v2/top-headlines/sources?apiKey=${API_KEY}`;
 export const NewsProvider = ({ children }) => {
   const [category, setCategory] = useState("");
   const [country, setCountry] = useState("");
+  console.log(`${process.env.REACT_APP_KEY}`);
+  const getData = async () => {
+    const response = await fetch(url);
+    console.log("response:", response);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   console.log("category:", category);
   console.log("my api key:", url);
   console.log("country", country);
