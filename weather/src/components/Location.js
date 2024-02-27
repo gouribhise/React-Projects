8991014128ab278471341a5ee96d4a9106b14aec
gouribhise <gouri.a.bhise@gmail.com>
2024-02-27 20:29:48 +0530

    displaying location data set

--- weather/src/components/Location.js
+++ weather/src/components/Location.js
@@ -1,7 +1,24 @@
 import React from "react";
 
-const Location = (props) => {
-  return <div>{props.name}</div>;
+const Location = ({ name, region, country, lat, lon, tz_id, localtime }) => {
+  return (
+    <div className="container py-5">
+      <div className="row">
+        <div className="col">
+          <div>
+            <h6>
+              {name} &nbsp;
+              {region}
+            </h6>
+            {lat} - {lon}
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">{localtime}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Location;
