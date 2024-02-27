0950ff490213d05299585089499f92a8689a5f1c
gouribhise <gouri.a.bhise@gmail.com>
2024-02-27 18:05:41 +0530

    fetching current weather

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,7 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const BASE_URL = "http://api.weatherapi.com/v1/current.json?";
+const API_KEY = process.env.REACT_APP_WEATHER_API;
 
 function App() {
+  const getData = () => {
+    axios
+      .get(`${BASE_URL}key=${API_KEY}&q=London`)
+      .then((data) => console.log(data.data))
+      .catch((error) => console.log(error));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <div>hello</div>
