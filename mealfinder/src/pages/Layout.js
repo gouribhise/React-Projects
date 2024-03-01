0f6b114425e6b715ecf36dcadd13d2c8cefb3085
gouribhise <gouri.a.bhise@gmail.com>
2024-03-01 20:15:36 +0530

    started meals project with chakra ui

---
+++ mealfinder/src/pages/Layout.js
@@ -0,0 +1,29 @@
+import React from "react";
+ import { Link as ReactRouterLink,Outlet} from 'react-router-dom'
+import { Link as ChakraLink, HStack, Heading, LinkProps } from '@chakra-ui/react'
+const Layout = () => {
+  return (
+    <>
+      
+      
+        <HStack spacing="125px">
+        <Heading p="24px">Meal Finder</Heading>
+
+        <ChakraLink as={ReactRouterLink} to='/'>
+  Home
+</ChakraLink>
+<ChakraLink as={ReactRouterLink} to='/'>
+  Home
+</ChakraLink>
+<ChakraLink as={ReactRouterLink} to='/'>
+  Home
+</ChakraLink>
+        </HStack>
+      
+
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
