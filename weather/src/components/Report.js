62928e6027f58a6e8cbf9fbac0bfacd192968084
gouribhise <gouri.a.bhise@gmail.com>
2024-03-01 13:54:19 +0530

    hide and show weatherdata

--- weather/src/components/Report.js
+++ weather/src/components/Report.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Report=({data})=>{
+    const[dayVisible,setDayVisible]=useState(false)
+const[astroVisible,setAstroVisible]=useState(false)
     return (
         <div className='col-lg-2'>
+          
             <h4>{data.date.slice(5,)}</h4>
+            <div className='row'>
+                <div className="container">
+                <button onClick={()=>setDayVisible(!dayVisible)} type='button'   className="btn btn-primary btn-block">Day</button>                
+                </div>
+            </div>
             
-         <table>
+
+{dayVisible?   <table>
             <tr>
                 <th>{data.day.condition.text}</th>
                 <td><img src={data.day.condition.icon}/></td>
@@ -14,20 +23,22 @@ const Report=({data})=>{
                 <th>UV</th>
                 <td>{data.day.uv}</td>
             </tr>
+       
             <tr>
-                <th> maxtemp_c </th>
+                <th>  Max C </th>
                 <td>{data.day.maxtemp_c}</td>
             </tr>
             <tr>
-                <th> maxtemp_f</th>
-                <td>{data.day.maxtemp_f}</td>
-            </tr>
-            <tr>
-                <th>mintemp_c</th>
+                <th>Min C</th>
                 <td>{data.day.mintemp_c}</td>
             </tr>
             <tr>
-                <th>mintemp_f</th>
+                <th> Max F</th>
+                <td>{data.day.maxtemp_f}</td>
+            </tr>
+         
+            <tr>
+                <th>Min F</th>
                 <td>{data.day.mintemp_f}</td>
             </tr>
             <tr>
@@ -44,7 +55,15 @@ const Report=({data})=>{
             </tr>
          </table>
 
-<table>
+:null}
+   <div className='row py-2'>
+    <div className='contaienr'>
+                <button onClick={()=>setAstroVisible(!astroVisible)} type="button" class="btn btn-primary btn-block">Astro</button>
+             
+                </div>
+            </div>
+{astroVisible?
+<table>      
     <tr>
         <th>  Sunrise </th>
         <td>{data.astro.sunrise}</td>
@@ -61,7 +80,7 @@ const Report=({data})=>{
         <th>  Moonset </th>
         <td>{data.astro.moonset}</td>
     </tr>
-</table>
+</table>:null}
      
    
 
